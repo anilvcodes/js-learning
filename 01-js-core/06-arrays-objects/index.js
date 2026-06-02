215eb7a53aa0f8096686454192e2078dfed80438
Dev-Aniweb <anilkumarve51@gmail.com>
2026-06-02 23:11:30 +0530

    array operation

---
+++ 01-js-core/06-arrays-objects/index.js
@@ -0,0 +1,12 @@
+let arr=[1,3,4,5,6,7];
+console.log(arr);
+
+//push at last
+   arr.push(23,23)
+   // remove last
+   arr.pop()
+   // remove strting 
+   arr.shift();
+// add starting
+arr.unshift(23,45)
+    console.log(arr);
