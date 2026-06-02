49358d306df3f02ad97f7c157445e8b9daed7246
Dev-Aniweb <anilkumarve51@gmail.com>
2026-06-02 23:33:06 +0530

    type-script

---
+++ 04-type-script/node-app/src/a.ts
@@ -0,0 +1,27 @@
+// const num:Number=100;
+
+// console.log(num);
+
+function greet (firstName:string){
+    console.log("hello" + firstName);
+
+}
+greet("anil");
+// sum
+function sum(a:number,b:number):number{
+    return a+b;
+}
+let val=sum(5,5);
+console.log(val);
+
+// vote 
+function age(num:number):boolean{
+    if(num>=18){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+let x=age(19);
+console.log(x);
