00ebe52ab2c47210c4566582db938afe0f98612e
Dev-Aniweb <anilkumarve51@gmail.com>
2026-06-12 06:54:50 +0530

    change

---
+++ 02-dsa-js/strings/index.js
@@ -0,0 +1,17 @@
+console.log("string");
+function reversString(str){
+    return str.split('').reverse().join('');
+}
+console.log(reversString("hello"));
+console.log(reversString("hello my name in anil k verma ").length);
+
+function isPalindrome(str3){
+    return str3===str3.split('').reverse().join('');
+}
+console.log(isPalindrome("madam"));
+
+
+function countstr(str4){
+    return str4.length
+}
+console.log(countstr("hello world"));
