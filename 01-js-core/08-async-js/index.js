120bc37ac666f72f5d0559015af7687224b80c5d
Dev-Aniweb <anilkumarve51@gmail.com>
2026-06-15 18:11:44 +0530

    change

--- 01-js-core/08-async-js/index.js
+++ 01-js-core/08-async-js/index.js
@@ -1,7 +1,25 @@
 // synchronous 
 
 console.log("hello world");
-for(let i=1;i<=1e9;i++){
+for(let i=1;i<=20;i++){
     console.log(i);
 }
 console.log("hello end ");
+
+// asynchronous
+console.log("hello listen");
+  setTimeout(()=>{
+    console.log("task1")
+  },1000);
+   setTimeout(()=>{
+    console.log("task2")
+   },3000);
+
+setTimeout(()=>{
+console.log("task")
+},500);
+setTimeout(()=>{
+console.log("task")
+},5000);
+
+console.log("hello end " );
--- 01-js-core/09-promises/index.js
+++ 01-js-core/09-promises/index.js
@@ -1,1 +1,13 @@
 console.log("promis here")
+
+function show(){
+    console.log("i am inside a function");
+}
+setTimeout(function(){
+    show();
+},2000);
+
+setTimeout(show,2000);
+setTimeout(function show(){
+    console.log("i am inside a function");
+},2000);
