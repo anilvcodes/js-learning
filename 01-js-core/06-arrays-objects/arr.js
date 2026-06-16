dce7dcc18f7853e828ce1ab048c027c175b72330
Dev-Aniweb <anilkumarve51@gmail.com>
2026-06-16 22:45:10 +0530

    change

--- 01-js-core/06-arrays-objects/arr.js
+++ 01-js-core/06-arrays-objects/arr.js
@@ -1,8 +1,22 @@
-console.log(" array");
- let arr= [12,23,56,45,4578];
- console.log(arr.length);
- console.log(arr);
- for(let i=0; i<=arr.length; i++){
-    console.log(arr[i]);
+// console.log(" array");
+//  let arr= [12,23,56,45,4578];
+//  console.log(arr.length);
+//  console.log(arr);
+//  for(let i=0; i<=arr.length; i++){
+//     console.log(arr[i]);
 
- }
+//  }
+
+ // flat
+
+let arr2 = [
+  12, 34, 56,
+  [345, 45],
+  [45, 34, 56, [45, [67, 67, 78], 67], 78, 54],
+  89, 90
+];
+
+console.log(arr2.flat());
+
+console.log(arr2.flat(Infinity));
+
