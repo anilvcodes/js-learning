5e09a888d7842e64f537460a7aa4307c7b9e99cb
Dev-Aniweb <anilkumarve51@gmail.com>
2026-07-09 00:13:26 +0530

    change '

--- 02-dsa-js/recursion/index.js
+++ 02-dsa-js/recursion/index.js
@@ -6,3 +6,24 @@ function fact(n){
     
 }
 console.log(fact(10));
+
+
+
+function count_down(n){
+    if(n==0){
+        console.log("done!")
+        return;
+    }
+    console.log(n);
+    count_down(n-1);
+}
+count_down(5)
+
+
+function fabon(n1){
+    if(n1<=1);{
+        return 1;
+    }
+    return  fabon(n1-1)+fabon(n1-2)
+}
+console.log(fabon(5));
