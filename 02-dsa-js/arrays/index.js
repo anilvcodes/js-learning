716f1d330c32abbd7cceebb788dfebb07ad2c9fd
Dev-Aniweb <anilkumarve51@gmail.com>
2026-08-03 17:58:13 +0530

    change

--- 02-dsa-js/arrays/index.js
+++ 02-dsa-js/arrays/index.js
@@ -17,3 +17,35 @@ let result = arr.map((value) => {
     return value * 5;
 });
 console.log(result);
+
+
+let products = [
+    {name:"Phone", price:20000, stock:true},
+    {name:"Laptop", price:60000, stock:false},
+    {name:"Watch", price:5000, stock:true}
+];
+
+
+
+let names = products.map(p => p.name);
+
+
+let available = products.filter(p => p.stock);
+
+
+
+let total = products.reduce((sum,p)=>sum+p.price,0);
+
+let phone = products.find(p=>p.name==="Phone");
+
+
+let sorted = products.sort((a,b)=>a.price-b.price);
+
+
+console.log(names);
+console.log(available);
+console.log(total);
+console.log(phone);
+console.log(sorted);
+
+
