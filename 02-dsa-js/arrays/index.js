e071d8559c86c3aa727edf5de665085b4a6da0d7
Dev-Aniweb <anilkumarve51@gmail.com>
2026-08-14 09:26:31 +0530

    change

--- 02-dsa-js/arrays/index.js
+++ 02-dsa-js/arrays/index.js
@@ -1,8 +1,5 @@
 let arr = [10, 20, 30, 40, 50];
 
-
-console.log(arr[0]);
-console.log(arr[1])
 arr.push(23,23);
 console.log(arr);
 
@@ -13,39 +10,72 @@ console.log(arr);
 for(let i=0 ; i<arr.length;i++){
     console.log(arr[i]);
 }
+
+
+// sum of array 
+let arr2 = [23, 45, 67, 78, 98];
+let sum = 0;
+
+for (let i = 0; i < arr2.length; i++) {
+    sum = sum + arr2[i];
+
+}
+
+console.log(sum);
+
 let result = arr.map((value) => {
     return value * 5;
 });
 console.log(result);
 
 
-let products = [
-    {name:"Phone", price:20000, stock:true},
-    {name:"Laptop", price:60000, stock:false},
-    {name:"Watch", price:5000, stock:true}
-];
 
 
 
-let names = products.map(p => p.name);
-
-
-let available = products.filter(p => p.stock);
 
 
 
-let total = products.reduce((sum,p)=>sum+p.price,0);
-
-let phone = products.find(p=>p.name==="Phone");
 
 
-let sorted = products.sort((a,b)=>a.price-b.price);
 
 
-console.log(names);
-console.log(available);
-console.log(total);
-console.log(phone);
-console.log(sorted);
+
+
+
+
+
+
+
+
+
+
+// let products = [
+//     {name:"Phone", price:20000, stock:true},
+//     {name:"Laptop", price:60000, stock:false},
+//     {name:"Watch", price:5000, stock:true}
+// ];
+
+
+
+// let names = products.map(p => p.name);
+
+
+// let available = products.filter(p => p.stock);
+
+
+
+// let total = products.reduce((sum,p)=>sum+p.price,0);
+
+// let phone = products.find(p=>p.name==="Phone");
+
+
+// let sorted = products.sort((a,b)=>a.price-b.price);
+
+
+// console.log(names);
+// console.log(available);
+// console.log(total);
+// console.log(phone);
+// console.log(sorted);
 
 
