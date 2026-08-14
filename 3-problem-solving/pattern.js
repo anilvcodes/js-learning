774ac06fce95fae9c7ba263567e6f863a885d6f9
Dev-Aniweb <anilkumarve51@gmail.com>
2026-08-14 08:39:15 +0530

    change

--- 3-problem-solving/pattern.js
+++ 3-problem-solving/pattern.js
@@ -53,3 +53,16 @@ for (let i = n; i >= 1; i--) {
 
     console.log(row);
 }
+
+
+
+
+
+
+for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <=n-i+1; j++) {
+        row += "* ";
+    }
+    console.log(row);
+}
