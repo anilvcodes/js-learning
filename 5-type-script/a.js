7eadb5a5c830406ffdce02ef5c41deb0862bf32a
Dev-Aniweb <anilkumarve51@gmail.com>
2026-08-19 17:34:54 +0530

    change

--- 5-type-script/a.js
+++ 5-type-script/a.js
@@ -2,6 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const x = 1;
 console.log(x);
+function greet(firstname) {
+    console.log(`hello namste ${firstname}`);
+}
+greet("anil ");
 function add(num1, num2) {
     return num1 + num2;
 }
--- 5-type-script/a.ts
+++ 5-type-script/a.ts
@@ -2,7 +2,15 @@ const x:number=1;
 
 console.log(x);
 
+// function in ts 
+function greet(firstname:string){
+    console.log(`hello namste ${firstname}`)
+}
+greet("anil ")
+
  function add(num1:number,num2:number)  {
     return num1+num2;
  }
  console.log(add(34,45));
+
+ 
