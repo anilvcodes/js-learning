4f7ca74ad6c403ff08dbbb53b0b87ef03fb14669
Dev-Aniweb <anilkumarve51@gmail.com>
2026-08-19 17:39:24 +0530

    change

--- 5-type-script/a.js
+++ 5-type-script/a.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const x = 1;
 console.log(x);
+// function in ts 
 function greet(firstname) {
     console.log(`hello namste ${firstname}`);
 }
@@ -10,4 +11,9 @@ function add(num1, num2) {
     return num1 + num2;
 }
 console.log(add(34, 45));
+//avg find in ts 
+function avg(num1, num2, num3) {
+    return (num1 + num2 + num3) / 3;
+}
+console.log(avg(25, 25, 25));
 //# sourceMappingURL=a.js.map
--- 5-type-script/a.ts
+++ 5-type-script/a.ts
@@ -13,4 +13,9 @@ greet("anil ")
  }
  console.log(add(34,45));
 
- 
+ //avg find in ts 
+ function avg(num1:number , num2:number,num3:number){
+    return (num1+num2+num3)/3;
+ }
+ console.log(avg(25,25,25));
+
