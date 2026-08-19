69b4ec65209d05875ff81b7aaae3a15ef6974f68
Dev-Aniweb <anilkumarve51@gmail.com>
2026-08-19 22:25:20 +0530

    change

--- 05-type-script/src/a.ts
+++ 05-type-script/src/a.ts
@@ -1,35 +1,65 @@
-const x:number=1; 
+// const x:number=1; 
 
-console.log(x);
+// console.log(x);
 
-// function in ts 
-function greet(firstname:string){
-    console.log(`hello namste ${firstname}`)
+// // function in ts 
+// function greet(firstname:string){
+//     console.log(`hello namste ${firstname}`)
+// }
+// greet("anil ")
+
+//  function add(num1:number,num2:number)  {
+//     return num1+num2;
+//  }
+//  console.log(add(34,45));
+
+//  //avg find in ts 
+//  function avg(num1:number , num2:number,num3:number){
+//     return (num1+num2+num3)/3;
+//  }
+//  console.log(avg(25,25,25));
+
+//  // age greater than 18 or not 
+
+//  function ageGreater(age:number) :boolean
+//  {
+//     if(age>18){
+//       return true;
+//     }
+//     else{
+//         return false;
+//     }
+//  }
+//  let age = ageGreater(7);
+//  console.log(age);
+
+//interface
+interface User{
+   firstName:string;
+   lastName:string;
+   age:number;
+   email?:string;
 }
-greet("anil ")
 
- function add(num1:number,num2:number)  {
-    return num1+num2;
- }
- console.log(add(34,45));
-
- //avg find in ts 
- function avg(num1:number , num2:number,num3:number){
-    return (num1+num2+num3)/3;
- }
- console.log(avg(25,25,25));
-
- // age greater than 18 or not 
-
- function ageGreater(age:number) :boolean
- {
-    if(age>18){
+function isLegal(user:User){
+   if (user.age>18){
       return true;
-    }
-    else{
-        return false;
-    }
- }
- let age = ageGreater(7);
- console.log(age);
+   }else{
+      return false
+   }
+}
+function greet(user:User){
+   console.log("hi there "+ user.firstName+ user.age);
 
+}
+
+
+console.log(isLegal({
+   firstName:"anil",
+   lastName:"veerma",
+   age:20
+}))
+greet({
+   firstName:"anil",
+   lastName:"veerma",
+   age:20})
