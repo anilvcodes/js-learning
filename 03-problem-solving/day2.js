5fbe577a8582230e5e11ba06196cbfa07c0f308d
Dev-Aniweb <anilkumarve51@gmail.com>
2026-08-27 22:18:06 +0530

    change

--- 03-problem-solving/day2.js
+++ 03-problem-solving/day2.js
@@ -13,3 +13,16 @@ addtocart("pen")
 addtocart("book")
 addtocart("book- cse ")
 console.log(cart.length);
+
+
+//Day 2: Even/odd checker banao.
+
+function oddOrEven(num){
+    if(num%2===0){
+        console.log("even number");}
+        else{
+            console.log("odd number");
+        }
+    }
+oddOrEven(23);
+oddOrEven(22)
