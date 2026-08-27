6ea37e804b5ca0661a05a9737941f2eedbc347f7
Dev-Aniweb <anilkumarve51@gmail.com>
2026-08-27 22:14:19 +0530

    change

--- 03-problem-solving/day1.js
+++ 03-problem-solving/day1.js
@@ -16,3 +16,15 @@ console.log(calculateReward(1));
 
 
 console.log(calculateReward(1000));
+
+//Day 1: User adult hai ya minor check karo.
+function adultOrMinor(age){
+  if (age>18){
+    console.log("adult")
+  }
+  else{
+    console.log("minor");
+  }
+}
+adultOrMinor(34);
+adultOrMinor(15)
